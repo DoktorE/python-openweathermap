dd81afc94736878194def3628b71fa4d28a46279
doktore <thisemail@mailtothis.com>
2015-12-27 16:16:53 +0200

    added getWeatherCity and getWeatherCityId

--- openweathermap/client.py
+++ openweathermap/client.py
@@ -9,19 +9,19 @@ class Client(object):
 			url += '/'
 		self.url = url
 
-	def _request(self, endpoint, method, data=None, **kwargs):
+	def _request(self, endpoint, method, appid='', data=None, **kwargs):
 		"""
 		Function to handle GET and POST requests
 
 		:param endpoint: endpoint for the API
 		:param method: method type for request
+		:param appid: API key
 		:param data: POST data
 		:param kwargs: extra arguments
 
 		:return: request response
 		"""
-
-		total_url = self.url + endpoint
+		total_url = self.url + endpoint + '&appid=' + appid
 
 		r = self.session
 
@@ -51,7 +51,6 @@ class Client(object):
 
 		:return: GET response
 		"""
-
 		return self._request(endpoint, 'get', **kwargs)
 
 	def _post(self, endpoint, data, **kwargs):
@@ -64,18 +63,35 @@ class Client(object):
 
 		:return: POST response
 		"""
-
 		return self.request(endpoint, 'post', data, **kwargs)
 
-	def getWeatherZip(self, zip_code, country_code, appid=''):
+	def getWeatherCity(self, city_name, country_code=None):
+		"""
+		Function to get the weather based on the name of a city
+
+		:param city_name: name of city to pass into the request
+		:param country_code: two letter code (ex: 'us') to pass into the request
+
+		:return: weather from request
+		"""
+		return self._get('data/2.5/weather?q=', city_name + ',' + country_code)
+
+	def getWeatherCityId(self, city_id):
+		"""
+		Function to get the weather based on it's city id (refer to OpenWeatherMap's own documentation to get a list of city ids)
+
+		:param city_id: city id of city
+		:return: weather from request
+		"""
+		return self._get('data/2.5/weather?id=', city_id);
+
+	def getWeatherZip(self, zip_code, country_code):
 		"""
 		Function to get the weather based on a zip code
 
 		:param zip_code: zip code to pass into the request
 		:param country_code: two letter code (ex: 'us') to pass into the request
-		:param appid: api key to pass into the request
 
 		:return: weather from request
 		"""
-
-		return self._get('/data/2.5/weather?zip=' + zip_code + ',' + country_code, + '&appid=' + appid)
+		return self._get('/data/2.5/weather?zip=' + zip_code + ',' + country_code)
